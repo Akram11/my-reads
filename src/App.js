fed1e622696a5f9bb3a119e76616d3a4d10ddbc7
Akram <akram.alfawakheeri@bms.bmsoft.de>
2019-04-24 20:32:48 +0200

    code:  delete togglepage function that is not used anymore

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Library from './components/Library'
-import SearchBook from './components/SearchBooks'
 import {Route} from 'react-router-dom'
-import SearchBooks from './components/SearchBooks';
+import SearchBooks from './components/SearchBooks'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+   Books: []
    
   }
-  
 
-  togglePage = () => { 
-    this.state.showSearchPage ? 
-   (this.setState(() => ({showSearchPage : false})))  :  (this.setState(() => ({showSearchPage : true})))
-  }
+
+  // componentDidMount(){
+  //   getAll().then(()=>{
+  //     setState( {books:  })
+  //   })
+  // }
+  
  
 
   render() {  
