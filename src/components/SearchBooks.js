d8f4810771faad6c6e808bc98aeaa10a8bf93c13
Akram <akram.alfawakheeri@bms.bmsoft.de>
2019-04-25 17:44:24 +0200

    fix: fixed showing pictures of the books

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import SearchBar from './SearchBar'
+import Book from './Book'
 
 class SearchBooks extends React.Component{
 
@@ -15,25 +16,7 @@ class SearchBooks extends React.Component{
             <div className="search-books-results">
               <ol className="books-grid">
               {books.map((book)=>  (
-              
-              <li key = {book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(${book.imageLinks.smallThumbnail})' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="move" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{ book.authors.join(', ') }</div>
-              </div>
-            </li>
+                  <Book book = {book}/>
               ))}
               </ol>
             </div>
