86a1a6fdbe334c8363240790adeb625aad14f2ac
Akram11 <akram.f11@gmail.com>
2019-05-16 17:55:31 +0200

    added the right functionality for the search page

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,55 +1,78 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import Book from './Book'
+import * as BooksAPI from '../BooksAPI'
 
-class SearchBooks extends React.Component{
+class SearchBooks extends React.Component {
 
-  
+
   state = {
-    query : '',
+    query: '',
+    resultBooks : []
   }
-  
 
-  handleInputChange =  (e) => {
-    this.setState({query : e.target.value})
+
+  handleInputChange = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+    if (query !== '') {
+      this.search(query)
+    }
   }
 
   // I'm using the method represented in the Contacts example of the course to search books by title
   // I think it's better than using pre-defined seach terms - hopefully it's accepted 
-  
 
-    render(){
-    const  { books, changeShelf } = this.props;
-    const  {query} = this.state;
+  search = query => {
+    BooksAPI.search(query)
+      .then((books) => {
+        this.setState(() => ({
+          resultBooks: books
+        }))
+      }
+      )
+  }
 
 
-  // if there's not query entered by the user show all the books,
-  // otherwise filter only the books whose title [or part of it] match the query 
 
-    const showingBooks = (query === '')
-    ? books
-    : books.filter((book) => (
-        book.title.toLowerCase().includes(query.toLowerCase())
-      ))
 
-        return(
-          <div>
-            <div className="search-books">
-              <SearchBar query={query} handleInputChange = {this.handleInputChange}/>
-            <div className="search-books-results">
-            {showingBooks.length === books.length ? (
-              <span>Showing all The books in the Library</span>
-            ):( <span>Showing {showingBooks.length} of {books.length}</span>)}
-              <ol className="books-grid">
-              {showingBooks.map((book)=>  (
-                  <Book key= {book.id} book = {book}  changeShelf = {changeShelf}/>
+  render() {
+    const { books, changeShelf } = this.props;
+    const { query, resultBooks } = this.state;
+    
+
+    //console.log(query)
+    //this.search(query)
+    //console.log(this.state.resultBooks)
+
+
+    // if there's not query entered by the user show all the books,
+    // otherwise filter only the books whose title [or part of it] match the query 
+
+
+
+    // (query === '')
+    //   ? books
+    //   : books.filter((book) => (
+    //     book.title.toLowerCase().includes(query.toLowerCase())
+    //   ))
+
+    return (
+      <div>
+        <div className="search-books">
+          <SearchBar query={query} handleInputChange={this.handleInputChange} />
+          <div className="search-books-results">
+            <ol className="books-grid">
+              {resultBooks.map((book) => (
+                <Book key={book.id} book={book} changeShelf={changeShelf} />
               ))}
-              </ol>
-            </div>
-          </div>   
-     </div>
-        )
-    }
+            </ol>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 
