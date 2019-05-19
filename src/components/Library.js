4fa5966992a9d1ee77535d90ac85bf173f96e16e
Akram A <akram.f11@gmail.com>
2019-05-19 19:29:29 +0200

    changed some CSS

--- src/components/Library.js
+++ src/components/Library.js
@@ -8,8 +8,8 @@ class Library extends React.Component {
 
     render() {
 
-        // assign the books that are assigned to a certain shelf to variables to be passed 
-        // to shelves compononet to be rendered in the right shelf
+        /* assign the books that are assigned to a certain shelf to variables to be passed 
+           to the corresponding Shelf component  */
 
         const { books, changeShelf } = this.props;
         const read = books.filter((book) => book.shelf === 'read')
